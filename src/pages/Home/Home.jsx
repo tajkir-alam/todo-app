bfb5f4b26533b5d04a47e70a574a51c056729955
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2024-05-29 17:53:54 +0600

    incompleted task homepage component added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import Task from '../../component/Task/Task';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchTasks } from '../../redux/features/taskSlice';
+import React from 'react';
+import IncompletedTask from './IncompletedTask';
 
 const Home = () => {
-    const dispatch = useDispatch();
-    const tasks = useSelector(state => state.tasks.tasks);
-    const [showIncompletedTask, setShowIncompletedTask] = useState(3)
-
-    useEffect(() => {
-        dispatch(fetchTasks());
-    }, [dispatch]);
+   
 
     return (
         <>
@@ -19,33 +11,9 @@ const Home = () => {
             </header>
             <main className='mt-5 space-y-3'>
                 <h5 className='text-center text-lg text-[#383670] font-medium tracking-wider'>In-completed Task</h5>
-                <div className="bg-white rounded p-5">
+                <div className="bg-white rounded p-5 pt-8">
                     {/* task card */}
-                    <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-                        {
-                            tasks.filter((task) => task.status === 'in-completed').slice(0, showIncompletedTask).map(({ id, taskTitle, taskDescription, status, deadLines, priority }) => (
-                                <Task key={id} taskTitle={taskTitle} taskDescription={taskDescription} status={status} deadLines={deadLines} priority={priority} />
-                            ))
-                        }
-                    </div>
-                    <div className='flex justify-end'>
-                        {
-                            showIncompletedTask === 3 ?
-                                <button
-                                    onClick={() => setShowIncompletedTask(tasks.filter((task) => task.status === 'in-completed').length)}
-                                    className='mt-5 bg-blue-900 hover:bg-blue-700 px-5 py-1 rounded-lg text-white font-medium tracking-wider'
-                                >
-                                    View All
-                                </button>
-                                :
-                                <button
-                                    onClick={() => setShowIncompletedTask(3)}
-                                    className='mt-5 bg-blue-900 hover:bg-blue-700 px-5 py-1 rounded-lg text-white font-medium tracking-wider'
-                                >
-                                    View Less
-                                </button>
-                        }
-                    </div>
+                   <IncompletedTask />
                 </div>
             </main>
         </>
---
+++ src/pages/Home/IncompletedTask.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState }  from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Task from '../../component/Task/Task';
+import { fetchTasks } from '../../redux/features/taskSlice';
+
+const IncompletedTask = () => {
+    const dispatch = useDispatch();
+    const tasks = useSelector(state => state.tasks.tasks);
+    const [showIncompletedTask, setShowIncompletedTask] = useState(3)
+
+    useEffect(() => {
+        dispatch(fetchTasks());
+    }, [dispatch]);
+
+    return (
+        <>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    tasks.filter((task) => task.status === 'in-completed').slice(0, showIncompletedTask).map(({ id, taskTitle, taskDescription, status, deadLines, priority }) => (
+                        <Task key={id} taskTitle={taskTitle} taskDescription={taskDescription} status={status} deadLines={deadLines} priority={priority} />
+                    ))
+                }
+            </div>
+            <div className='flex justify-end'>
+                {
+                    showIncompletedTask === 3 ?
+                        <button
+                            onClick={() => setShowIncompletedTask(tasks.filter((task) => task.status === 'in-completed').length)}
+                            className='mt-5 bg-blue-900 hover:bg-blue-700 px-5 py-1 rounded-lg text-white font-medium tracking-wider'
+                        >
+                            View All
+                        </button>
+                        :
+                        <button
+                            onClick={() => setShowIncompletedTask(3)}
+                            className='mt-5 bg-blue-900 hover:bg-blue-700 px-5 py-1 rounded-lg text-white font-medium tracking-wider'
+                        >
+                            View Less
+                        </button>
+                }
+            </div>
+        </>
+    );
+};
+
+export default IncompletedTask;
