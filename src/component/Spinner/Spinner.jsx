37bf89bee09a2974c0a4fdac1e4b7005b4013fbb
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2024-06-02 13:32:35 +0600

    spinner position updated

--- src/component/Spinner/Spinner.jsx
+++ src/component/Spinner/Spinner.jsx
@@ -5,7 +5,7 @@ const Spinner = () => {
     const { loading } = useSelector((state) => state.tasks)
 
     return (
-        loading && <div className="absolute top-5 inset-x-1/2 w-10 h-10 animate-[spin_1s_linear_infinite] rounded-full border-4 border-r-white border-sky-400 z-50"></div>
+        loading && <div className="fixed top-1/2 inset-x-1/2 w-16 h-16 animate-[spin_1s_linear_infinite] rounded-full border-4 border-r-white border-sky-400 z-50"></div>
     );
 };
 
