b0b3a6ca34bd7343a33a7604f78c078fcbefedfa
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2024-06-03 22:37:50 +0600

    modal updated and separeted for PATCH and POST. also showing select values

--- src/component/Modal/Modal.jsx
+++ src/component/Modal/Modal.jsx
@@ -8,7 +8,7 @@ import { addNewTask, updateTask } from '../../redux/features/taskSlice';
 
 const Modal = () => {
     const dispatch = useDispatch();
-    const { isOpen, modalFor } = useSelector((state) => state.modal)
+    const { isOpen, modalFor } = useSelector((state) => state.modal);
     const { loading } = useSelector((state) => state.tasks);
 
     const optionsPriority = [
@@ -18,87 +18,77 @@ const Modal = () => {
     ];
 
     const optionsStatus = [
-        { value: 'completed', label: 'completed' },
-        { value: 'in-completed', label: 'in-completed' },
+        { value: 'completed', label: 'Completed' },
+        { value: 'in-completed', label: 'In-completed' },
     ];
 
-    // getting the data which have to be update
     const getInfoFromLocalStorage = JSON.parse(localStorage.getItem('updateTaskInfo'));
-    let initialValues = {
+
+    const initialValues = {
         taskTitle: '',
         taskDescription: '',
         priority: '',
         status: '',
         deadLines: '',
     };
-    if (getInfoFromLocalStorage && modalFor === 'updateTask') {
-        initialValues = {
-            taskTitle: getInfoFromLocalStorage.taskTitle || '',
-            taskDescription: getInfoFromLocalStorage.taskDescription || '',
-            status: optionsStatus.find(option => option.value === getInfoFromLocalStorage?.status) || '',
-            priority: optionsPriority.find(option => option.value === getInfoFromLocalStorage?.priority) || '',
-            deadLines: getInfoFromLocalStorage.deadLines || '',
-        };
-    };
 
-    // setting priority and status value for update task
-    const [priorityValue, setPriorityValue] = useState(optionsPriority.find(option => option.value === getInfoFromLocalStorage?.priority) || null);
-    const [statusValue, setStatusValue] = useState(optionsStatus.find(option => option.value === getInfoFromLocalStorage?.status) || null);
+    const validationSchema = Yup.object({
+        taskTitle: modalFor === 'addTask' ? Yup.string().required('Title is required') : Yup.string(),
+        taskDescription: modalFor === 'addTask' ? Yup.string().required('Description is required') : Yup.string(),
+        priority: modalFor === 'addTask' ? Yup.string().oneOf(['low', 'medium', 'high']).required('Please choose task priority') : Yup.string().oneOf(['low', 'medium', 'high']),
+        status: modalFor === 'addTask' ? Yup.string().oneOf(['completed', 'in-completed']).required('Please choose task status') : Yup.string().oneOf(['completed', 'in-completed']),
+        deadLines: modalFor === 'addTask' ? Yup.date().required('Please select a deadline for the task') : Yup.date(),
+    });
 
-    // rendering modal to set the value for update task and make the state null for add task
-    useEffect(() => {
-        if (modalFor === 'updateTask') {
-            setPriorityValue(optionsPriority.find(option => option.value === getInfoFromLocalStorage?.priority));
-            setStatusValue(optionsStatus.find(option => option.value === getInfoFromLocalStorage?.status));
-        }
-        else if (modalFor !== 'updateTask') {
-            setPriorityValue(null)
-            setStatusValue(null)
-        }
-    }, [modalFor, getInfoFromLocalStorage?.priority, getInfoFromLocalStorage?.status])
+    const [priorityValue, setPriorityValue] = useState(null);
+    const [statusValue, setStatusValue] = useState(null);
 
-    const { handleSubmit, handleChange, resetForm, errors, values, touched, setFieldValue } = useFormik({
+    const formik = useFormik({
         initialValues,
-        validationSchema: Yup.object({
-            taskTitle: Yup.string().required('Title is required'),
-            taskDescription: Yup.string().required('Description is required'),
-            priority: Yup.string().oneOf(['low', 'medium', 'high']).required('Please chose task priority'),
-            status: Yup.string().oneOf(['completed', 'in-completed']).required('Please chose task priority'),
-            deadLines: Yup.date().required('Please select a dead-line for the task'),
-        }),
+        validationSchema,
         onSubmit: async (values) => {
-            // Submitting form conditionally 
             if (modalFor === 'addTask') {
-                try {
-                    dispatch(addNewTask(values));
-                    resetForm()
-                    dispatch(closeModal());
-                } catch (error) {
-                    console.error('There was a problem with the fetch operation:', error);
-                }
+                dispatch(addNewTask(values));
             } else if (modalFor === 'updateTask') {
-                try {
-                    dispatch(updateTask({ id: getInfoFromLocalStorage.id, taskData: values }));
-                    resetForm()
-                    dispatch(closeModal());
-                } catch (error) {
-                    console.error('There was a problem with the fetch operation:', error);
-                }
+                dispatch(updateTask({ id: getInfoFromLocalStorage.id, taskData: values }));
             }
+            formik.resetForm();
+            dispatch(closeModal());
         },
     });
 
+    // destructuring from formik to use globally.
+    const { handleSubmit, handleChange, resetForm, errors, values, touched, setFieldValue, setValues } = formik;
+
+    // rendering to select status and priority and remove input caches and making filed's empty
+    useEffect(() => {
+        if (modalFor === 'updateTask' && getInfoFromLocalStorage) {
+            setValues({
+                taskTitle: getInfoFromLocalStorage?.taskTitle || '',
+                taskDescription: getInfoFromLocalStorage?.taskDescription || '',
+                priority: getInfoFromLocalStorage?.priority || '',
+                status: getInfoFromLocalStorage?.status || '',
+                deadLines: getInfoFromLocalStorage?.deadLines || '',
+            });
+            setPriorityValue(optionsPriority.find(option => option.value === getInfoFromLocalStorage?.priority));
+            setStatusValue(optionsStatus.find(option => option.value === getInfoFromLocalStorage?.status));
+        } else if (modalFor === 'addTask') {
+            resetForm();
+            setPriorityValue(null);
+            setStatusValue(null);
+        }
+    }, [modalFor, getInfoFromLocalStorage?.priority, getInfoFromLocalStorage?.status, setValues, resetForm]);
 
     return (
         <div onClick={() => dispatch(closeModal())} className={`fixed top-0 left-0 w-full h-full bg-[#0303037e] backdrop-blur-sm z-50 ${isOpen ? 'visible opacity-100' : 'invisible opacity-0'} duration-200`}>
             <form onClick={(e) => e.stopPropagation()} onSubmit={handleSubmit} className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-white w-5/6 rounded-md p-5">
-                <p className='text-[26px] text-center tracking-widest mb-5'>{modalFor === 'addTask' ? 'Add' : modalFor === 'updateTask' ? 'Update' : null} Task</p>
+                <p className='text-[26px] text-center tracking-widest mb-5'>{modalFor === 'addTask' ? 'Add' : 'Update'} Task</p>
                 <label htmlFor="taskTitle" className='text-sm block mb-2 font-medium text-slate-800 tracking-widest'>Task Title</label>
                 <input
                     id='taskTitle'
                     type="text"
                     onChange={handleChange}
-                    defaultValue={initialValues.taskTitle}
+                    value={values.taskTitle}
                     className='px-2 py-3 w-full border-[1px] border-[#e5e5e5] focus:border-[#047CEB] mb-5 focus:outline-none rounded'
                 />
                 {touched.taskTitle && errors.taskTitle ? (
@@ -110,7 +100,7 @@ const Modal = () => {
                     id='taskDescription'
                     type="text"
                     onChange={handleChange}
-                    defaultValue={initialValues.taskDescription}
+                    value={values.taskDescription}
                     className='px-2 py-3 w-full border-[1px] border-[#e5e5e5] focus:border-[#047CEB] mb-5 focus:outline-none rounded'
                 />
                 {touched.taskDescription && errors.taskDescription ? (
@@ -123,8 +113,8 @@ const Modal = () => {
                         <Select
                             value={priorityValue}
                             onChange={(e) => {
-                                setFieldValue('priority', e.value)
-                                setPriorityValue(optionsPriority.find(option => option.value === e.value))
+                                setFieldValue('priority', e.value);
+                                setPriorityValue(optionsPriority.find(option => option.value === e.value));
                             }}
                             options={optionsPriority}
                             isSearchable={false}
@@ -140,8 +130,8 @@ const Modal = () => {
                         <Select
                             value={statusValue}
                             onChange={(e) => {
-                                setFieldValue('priority', e.value)
-                                setStatusValue(optionsStatus.find(option => option.value === e.value))
+                                setFieldValue('status', e.value);
+                                setStatusValue(optionsStatus.find(option => option.value === e.value));
                             }}
                             options={optionsStatus}
                             isSearchable={false}
@@ -159,7 +149,7 @@ const Modal = () => {
                     type="date"
                     name='deadLines'
                     onChange={handleChange}
-                    defaultValue={initialValues.deadLines}
+                    value={values.deadLines}
                     className='px-2 py-3 w-full border-[1px] border-[#e5e5e5] focus:border-[#047CEB] mb-5 focus:outline-none rounded'
                 />
                 {touched.deadLines && errors.deadLines ? (
